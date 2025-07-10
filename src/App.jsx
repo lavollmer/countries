295413cef469f3adaf06e5b727e63ec4d03b7e6b
Laura <laura.e.vollmer@gmail.com>
2025-07-10 10:32:50 -0500

    Updating the HTML and styling

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,18 @@ function App() {
 
   return (
     <>
-      <div className='background'>
+      <div className='background-page'>
         <div className='header'>
           <h1 className='title'>Where in the world?</h1>
         </div>
-        <h1>Country</h1>
+        <div className='background-body'>
+          <div>
+
+          </div>
+          <div>
+
+          </div>
+        </div>
       </div>
     </>
   )
