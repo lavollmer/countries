c0f6d191f6d6771b1d547210a5b87fc6963bc5c1
Laura <laura.e.vollmer@gmail.com>
2025-07-11 10:45:40 -0500

    Creating the filter dropdown menu

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import { CiSearch } from "react-icons/ci";
 
 function App() {
   const [country, setCountry] = useState('')
+  const [seletectedFilter, setSelectedFilter] = useState('')
+  const [originalData, setOriginalData] = useState([])
+  const [filteredData, setFilteredData] = useState(originalData)
 
   return (
     <>
@@ -19,8 +22,15 @@ function App() {
                 <input placeholder='Search for a country...' type='text' value={country} />
               </button>
             </div>
+            {/* Filter Dropdown Menu */}
             <div>
-              <h1>Filter</h1>
+              <div>
+                <select value={selectedFilter} onChange={handleFilterChange}>
+                  <option value="">France</option>
+                  <option value="">France</option>
+                  <option value="">France</option>
+                </select>
+              </div>
             </div>
           </div>
         </div>
