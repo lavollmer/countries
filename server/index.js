4091a0f55be8bd28fddc504f3b6473cb13bb5b04
Laura <laura.e.vollmer@gmail.com>
2025-07-12 06:23:14 -0500

    Add index.js

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios')
+require('dotenv').config()
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('api/country',async(req,res) => {
+    const country = req.query.query;
+    const apiKey = process.env.VITE_REACT_APP_COUNTRY;
+
+    try {
+        const response = await axios.get('')
+        res.json(response.data);
+    } catch {
+        console.error('Error fetching from API:', error.message)
+        res.status(500).json({error:'Failed to fetch location data'});
+    }
+})
+
+app.listen(PORT, () => {
+    console.log('Server running on 'http://localhost:${PORT}')
+})
+
