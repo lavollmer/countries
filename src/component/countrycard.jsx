d60956293a954f072a8831d595b35837f9cc086b
Laura <laura.e.vollmer@gmail.com>
2025-07-22 14:27:44 -0500

    Updating the styling of the country cards

--- src/component/countrycard.jsx
+++ src/component/countrycard.jsx
@@ -1,30 +1,31 @@
 import '../App.css';
 
-function CountryCard ({country, imageUrl, population, region, capital}) {
-    return (
-        <div className='countries-card'>
-          <div className='country-image'>
-            <h1>Image Flag</h1>
-            <img src={imageUrl} alt={`Flag of ${country}`} />
-          </div>
-          <div className='country-name'>
-            <h1>{country}</h1>
-          </div>
-          <div className='country-detail-row'>
-            <p className='country-detail'>Population: </p>
-            <p>{population}</p>
-          </div>
-          <div className='country-detail-row'>
-            <p className='country-detail'>Region: </p>
-            <p>{region}</p>
-          </div>
-          <div className='country-detail-row'>
-            <p className='country-detail'>Capital: </p>
-            <p>{capital}</p>
-          </div>
-
+function CountryCard({ country, imageUrl, population, region, capital }) {
+  return (
+    <div className='countries-card'>
+      <div className='country-image'>
+        <h1>Image Flag</h1>
+        <img src={imageUrl} alt={`Flag of ${country}`} />
+      </div>
+      <div className='country-name'>
+        <h1>{country}</h1>
+      </div>
+      <div className='country-detail-card'>
+        <div className='country-detail-row'>
+          <p className='country-detail'>Population:  </p>
+          <p className='country-detail-actual'> {population}</p>
         </div>
-    )
+        <div className='country-detail-row'>
+          <p className='country-detail'>Region:  </p>
+          <p className='country-detail-actual'> {region}</p>
+        </div>
+        <div className='country-detail-row'>
+          <p className='country-detail'>Capital:  </p>
+          <p className='country-detail-actual'> {capital}</p>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default CountryCard;
