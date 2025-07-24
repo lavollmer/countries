cb7c4204885d11e11efdf4d6653867dacb9cd5fa
Laura <laura.e.vollmer@gmail.com>
2025-07-24 15:02:38 -0500

    Styling the button for the back action for the country page

--- src/component/countrypage.jsx
+++ src/component/countrypage.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import '../App.css';
+import { FaArrowLeft } from "react-icons/fa6";
 
 const countrypage = () => {
   return (
     <div className='background-page'>
       <div className='header'>
         <h1 className='title'>Where in the world?</h1>
+      </div>
+      <div className='button-container'>
+        <button className='button'><FaArrowLeft /> Back</button>
       </div>
     </div>
   )
