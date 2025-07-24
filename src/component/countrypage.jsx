a92e9c8c6a9e67edf2c4baac442966a77ff088d6
Laura <laura.e.vollmer@gmail.com>
2025-07-24 15:06:45 -0500

    Updating the localhost country page

--- src/component/countrypage.jsx
+++ src/component/countrypage.jsx
@@ -1,8 +1,7 @@
-import React from 'react'
 import '../App.css';
 import { FaArrowLeft } from "react-icons/fa6";
 
-const countrypage = () => {
+const countrypage = ({ imageUrl, country, population, region, capital }) => {
   return (
     <div className='background-page'>
       <div className='header'>
@@ -10,6 +9,28 @@ const countrypage = () => {
       </div>
       <div className='button-container'>
         <button className='button'><FaArrowLeft /> Back</button>
+      </div>
+      <div>
+        <div className='country-image'>
+          <img src={imageUrl} alt={`${country} flag`} className='country-flag' />
+        </div>
+        <div className='country-name'>
+          <h1>{country}</h1>
+        </div>
+        <div className='country-detail-card'>
+          <div className='country-detail-row'>
+            <p className='country-detail'>Population:  </p>
+            <p className='country-detail-actual'> {population}</p>
+          </div>
+          <div className='country-detail-row'>
+            <p className='country-detail'>Region:  </p>
+            <p className='country-detail-actual'> {region}</p>
+          </div>
+          <div className='country-detail-row'>
+            <p className='country-detail'>Capital:  </p>
+            <p className='country-detail-actual'> {capital}</p>
+          </div>
+        </div>
       </div>
     </div>
   )
