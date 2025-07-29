c56214914a9cc141927a29a12f1c9bd5045dfde1
Laura <laura.e.vollmer@gmail.com>
2025-07-29 11:40:01 -0500

    Styling the individual page

--- src/component/countrypage.jsx
+++ src/component/countrypage.jsx
@@ -33,18 +33,19 @@ const countrypage = () => {
           </div>
           <div className="individual-country-details">
             <h1 className='individual-country-name'>{item.name.common}</h1>
-            <div>
-              <p><strong>Population:</strong> {item.population.toLocaleString()}</p>
-              <p><strong>Region:</strong> {item.region}</p>
-              <p><strong>Sub Region:</strong> {item.subregion || 'N/A'}</p>
-              <p><strong>Capital:</strong> {item.capital?.[0] || 'N/A'}</p>
+            <div className='grid-container'>
+              <div className='individual-grid-item'>
+                <p><strong>Population:</strong> {item.population.toLocaleString()}</p>
+                <p><strong>Region:</strong> {item.region}</p>
+                <p><strong>Sub Region:</strong> {item.subregion || 'N/A'}</p>
+                <p><strong>Capital:</strong> {item.capital?.[0] || 'N/A'}</p>
+              </div>
+              <div className='individual-grid-item'>
+                <p><strong>Top Level Domain:</strong> {item.tld?.join(', ') || 'N/A'}</p>
+                <p><strong>Languages:</strong> {Object.values(item.languages || {}).join(', ') || 'N/A'}</p>
+                <p><strong>Currencies:</strong> {Object.values(item.currencies || {}).map(currency => `${currency.name} (${currency.symbol})`).join(', ') || 'N/A'}</p>
+              </div>
             </div>
-            <div>
-              <p><strong>Top Level Domain:</strong> {item.tld?.join(', ') || 'N/A'}</p>
-              <p><strong>Languages:</strong> {Object.values(item.languages || {}).join(', ') || 'N/A'}</p>
-              <p><strong>Currencies:</strong> {Object.values(item.currencies || {}).map(currency => `${currency.name} (${currency.symbol})`).join(', ') || 'N/A'}</p>
-            </div>
-            <h2>Border Countries:</h2>
           </div>
         </div>
       </div>
